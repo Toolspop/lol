34e7551b6835e8de4dd47c05de4b55075a20cb33
Toolspop <91412017+Toolspop@users.noreply.github.com>
2023-02-11 14:48:21 +0300

    Update kcgg.lua

--- kcgg.lua
+++ kcgg.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("КСГ СКРИПТ ХЕРОвый", "RJTheme3")
-local Tab = Window:NewTab("дефолтные функции")
+local Window = Library.CreateLib("КСГ СКРИПТ ХЕРОвый", "RJTheme1")
+local Tab = Window:NewTab("teleports")
 local Section = Tab:NewSection("мой дс: лантисор")
 Section:NewLabel("телепортв:") 
 Section:NewButton("тп в комнату командира", "тп", function()
@@ -15,7 +15,17 @@ end)
 Section:NewButton("тп в kit kat room", "ButtonInfo", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-17.3059101, 17.7294502, 134.311234, 0, 0, 1, 0, 1, -0, -1, 0, 0)
 end)
-Section:NewLabel("разное")
+local Tab = Window:NewTab("tools")
+local Section = Tab:NewSection("подбор")
+
+Section:NewButton("тп все гиверы оружия к себе", "tp", function()
+for i,v in pairs(game:GetDescendants()) do
+if v.Name == 'giver' then
+v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+end
+end
+end)
+
 Section:NewButton("все туллсы", "ы", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-14.4494781, 12.2207336, 170.572754, -0.707313538, -0.00645517558, -0.706870973, -0.06160447, 0.996716797, 0.0525409542, 0.704211056, 0.0807093158, -0.705388904)
 wait(0.5)
@@ -37,6 +47,10 @@ game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-24.6298
 wait(0.5)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(67.95111846923828, 8.114665031433105, -75.37315368652344)
 end)
+
+local Tab = Window:NewTab("Разное")
+local Section = Tab:NewSection("мало")
+
 Section:NewButton("ESP", "есп лул", function()
 while wait(0.5) do
     for i, childrik in ipairs(workspace:GetDescendants()) do
@@ -57,6 +71,17 @@ while wait(0.5) do
     end
 end
 end)
+
+Section:NewButton("выключить портал", "работает плохо", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(181.444473, -69.2206268, 208.394928, -0.939700961, 0, 0.341998369, 0, 1, 0, -0.341998369, 0, -0.939700961)
+wait(0.3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(94.68738555908203, 39.67461395263672, -63.52935028076172)
+end)
+
+local Tab = Window:NewTab("Tp parts")
+local Section = Tab:NewSection("к себе телепортирует")
+
+
 Section:NewButton("тп nokia к себе", "ButtonInfo", function()
 for i,v in pairs(game:GetDescendants()) do
 if v.Name == 'nokia' then
@@ -92,11 +117,10 @@ v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 end
 end
 end)
-Section:NewButton("выключить портал", "работает плохо", function()
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(181.444473, -69.2206268, 208.394928, -0.939700961, 0, 0.341998369, 0, 1, 0, -0.341998369, 0, -0.939700961)
-wait(0.3)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(94.68738555908203, 39.67461395263672, -63.52935028076172)
-end)
+
+local Tab = Window:NewTab("нищие функции")
+local Section = Tab:NewSection("увы")
+
 Section:NewSlider("скорость", "спииид", 1000, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
@@ -117,6 +141,27 @@ Section:NewToggle("прозрачность нескольких партов", 
         game:GetService("Workspace").Union.CanCollide = true
     end
 end)
+local Tab = Window:NewTab("новые функции")
+local Section = Tab:NewSection("топ!")
+
+Section:NewButton("фарм монет", "ы", function()
+for i =  1, 10 do
+wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(196.59335327148438, 6.40556001663208, 100.56067657470703)
+wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(167.5731201171875, 6.405562400817871, 47.92317199707031)
+wait(0.2)
+end
+end)
+
+Section:NewButton("autocliker не мой", "ы", function()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/JustEzpi/ROBLOX-Scripts/main/ROBLOX_AutoClicker"))()
+end)
+
 local Tab = Window:NewTab("крейдит")
 local Section = Tab:NewSection("лантисор")
 Section:NewLabel("скрипт кал")
+Section:NewLabel("мог и лучше сделать если бы знал луа хорошо")
+Section:NewLabel("но даже эти функции могут испортить игру")
+Section:NewLabel("возможно будет сделан дс сервер")
+Section:NewLabel("ксг хорошая игра , но у разрабов нет времени")
